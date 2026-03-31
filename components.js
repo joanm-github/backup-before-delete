6ce93f0d645f9aa8f185dc175ca7be0aa261d020
Joan Montiel <joanmontiel@MacBookBPdeJoan.lan>
2026-03-31 19:06:36 +0200

    Fix anima_nera logo transparency and increase size

--- components.js
+++ components.js
@@ -173,8 +173,8 @@ const Footer = () => `
             <!-- Partner Logos -->
             <div class="flex items-center gap-8 ml-4">
                 <img src="./assets/logos/suoni_rari_color.png" alt="Suoni Rari" class="h-8 md:h-12 w-auto transition-all">
-                <a href="https://animanera.net/" target="_blank" class="flex items-center group -translate-y-1">
-                    <img src="./assets/logos/anima_nera.png" alt="Anima Nera" class="h-20 md:h-28 w-auto opacity-70 group-hover:opacity-100 transition-all mix-blend-screen">
+                <a href="https://animanera.net/" target="_blank" class="flex items-center group -translate-y-2 mix-blend-screen">
+                    <img src="./assets/logos/anima_nera.png" alt="Anima Nera" class="h-24 md:h-32 w-auto opacity-80 group-hover:opacity-100 transition-all">
                 </a>
             </div>
         </div>
